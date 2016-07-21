b0dad4d520917c52da870e7628224168ee50ed24
gabe <gj@users.noreply.github.com>
2016-07-21 15:14:13 -0400

    added placeholder strings to prevent infinite loop

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,17 +1,24 @@
 def select_books_titles_and_years_in_first_series_order_by_year
+  "Write your SQL query here"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
+  "Write your SQL query here"
 end
 
+
 def select_value_and_count_of_most_prolific_species
+  "Write your SQL query here"
 end
 
 def select_name_and_series_subgenres_of_authors
+  "Write your SQL query here"
 end
 
 def select_series_title_with_most_human_characters
+  "Write your SQL query here"
 end
 
 def select_character_names_and_number_of_books_they_are_in
+  "Write your SQL query here"
 end
